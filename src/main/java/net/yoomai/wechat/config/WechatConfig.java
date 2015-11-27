551f929fb19191841d239d03a6b0a20bbc5a98f8
coffeefoam <coffeefoam@126.com>
2015-11-27 12:57:44 +0800

    http相关操作以及配置项

--- src/main/java/net/yoomai/wechat/config/WechatConfig.java
+++ src/main/java/net/yoomai/wechat/config/WechatConfig.java
@@ -11,12 +11,23 @@ package net.yoomai.wechat.config;
  * @(#)AppConfig.java 1.0 27/11/2015
  */
 public interface WechatConfig {
+    /*
+     * 微信商户账号
+     */
     public static final String _WX_MCHID_ = "";
 
+    /*
+     * 微信商户证书位置
+     */
     public static final String _WX_MCHID_PKCS_ = "";
 
-
+    /*
+     * XML格式的数据
+     */
     public static final String _DATA_XML_ = "xml";
 
+    /*
+     * JSON格式的数据
+     */
     public static final String _DATA_JSON_ = "json";
 }
