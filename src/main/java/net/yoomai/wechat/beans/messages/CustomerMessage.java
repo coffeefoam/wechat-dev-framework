f39fc147372bcf163189fd7e8c68d6411c70125f
coffeefoam <coffeefoam@126.com>
2015-11-28 14:39:00 +0800

    增加消息对象

--- src/main/java/net/yoomai/wechat/beans/messages/CustomerMessage.java
+++ src/main/java/net/yoomai/wechat/beans/messages/CustomerMessage.java
@@ -11,5 +11,27 @@ package net.yoomai.wechat.beans.messages;
  * @(#)CustomerMessage.java 1.0 28/11/2015
  */
 public class CustomerMessage implements Message {
+    private String touser;
+    private String msgtype;
 
+    public CustomerMessage(String touser, String msgtype) {
+        this.touser = touser;
+        this.msgtype = msgtype;
+    }
+
+    public String getTouser() {
+        return touser;
+    }
+
+    public void setTouser(String touser) {
+        this.touser = touser;
+    }
+
+    public String getMsgtype() {
+        return msgtype;
+    }
+
+    public void setMsgtype(String msgtype) {
+        this.msgtype = msgtype;
+    }
 }
