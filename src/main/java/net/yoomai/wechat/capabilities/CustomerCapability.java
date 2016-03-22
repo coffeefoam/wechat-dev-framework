4a40678d90de5c3069c7d2d9213141928be1b729
coffeefoam <coffeefoam@126.com>
2016-03-22 11:28:41 +0800

    nothing.

---
+++ src/main/java/net/yoomai/wechat/capabilities/CustomerCapability.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2014, wolaiyee.com. All rights reserved.
+ * wolaiyee.com. Use is subject to license terms.
+ */
+package net.yoomai.wechat.capabilities;
+
+/**
+ * 多客服处理能力
+ *
+ * @author Ray & coffeefoam@126.com & http://github.com/coffeefoam
+ * @(#)CustomerCapability.java 1.0 21/03/2016
+ */
+public class CustomerCapability extends AbstractCapability {
+
+    public CustomerCapability(String id) {
+        init(id);
+    }
+}
